ce9abf54db965ec8893392672517019a5cda9e3b
silkmaster25 <142832894+silkmaster25@users.noreply.github.com>
2023-08-21 20:02:03 -0700

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -196,11 +196,13 @@
         </div>
     </div>
     -->
-    <div id="currentChamp">
-        <h4>2022/23 League Champion</h4>
-        <div id="champ" on:click={() => {if(managers.length) gotoManager({year: podiums[0].year, leagueTeamManagers, rosterID: parseInt(podiums[0].champion)})}} >
-            <img src="{getAvatarFromTeamManagers(leagueTeamManagers, podiums[0].champion, podiums[0].year)}" class="first" alt="champion" />
-            <img src="./laurel.png" class="laurel" alt="laurel" />
+    <div class="leagueData">
+        <div id="currentChamp">
+            <h4>2022/23 League Champion</h4>
+            <div id="champ" on:click={() => {if(managers.length) gotoManager({year: podiums[0].year, leagueTeamManagers, rosterID: parseInt(podiums[0].champion)})}} >
+                <img src="{getAvatarFromTeamManagers(leagueTeamManagers, podiums[0].champion, podiums[0].year)}" class="first" alt="champion" />
+                <img src="./laurel.png" class="laurel" alt="laurel" />
+            </div>
         </div>
     </div>
 
